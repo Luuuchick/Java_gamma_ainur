c1f31b782207b586165a0fe24f6d31251cc7bd77
Luuuchick <a.nauryzbayeva13@gmail.com>
2024-05-23 10:16:45 +0300

    Задание 3

--- sandbox/src/main/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointTests.java
@@ -6,10 +6,10 @@ import org.testng.annotations.Test;
 
 public class PointTests {
     @Test
-         public void testArea(){
-        Point p4 =  new Point(0,0,3,4);
-       // assert p4.distance() == 5;
-        Assert.assertEquals(p4.distance(),5.0);
+    public void testArea() {
+        Point p4 = new Point(0, 0, 3, 4);
+        // assert p4.distance() == 5;
+        Assert.assertEquals(p4.distance(), 5.0);
     }
 
 }
