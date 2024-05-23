84fbdb911544c84630c59a9773daedc2042238a3
Luuuchick <a.nauryzbayeva13@gmail.com>
2024-05-23 11:23:24 +0300

    Update MyFirstProgram.java

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,15 +3,15 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-       Point p1 = new Point(0,0);
-        Point p2 = new Point(3,4);
-        Point p3 =  new Point(0,0,3,4);
-        System.out.println(distance(p1,p2));
-        System.out.println(p3.distance(p2));
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(3, 4);
+        System.out.println(distance(p1, p2));
+        System.out.println(p1.distance(p2));
     }
-    public static double distance(Point p1,Point p2){
+
+    public static double distance(Point p1, Point p2) {
         double dx = p2.x - p1.x;
-        double dy = p2.y-p1.y;
-       return Math.sqrt(dx*dx +dy*dy);
+        double dy = p2.y - p1.y;
+        return Math.sqrt(dx * dx + dy * dy);
     }
 }
