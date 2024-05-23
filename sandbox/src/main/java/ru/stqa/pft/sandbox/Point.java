77e93b304c18c5e0c2f940fe2a5786bb9e122034
Luuuchick <a.nauryzbayeva13@gmail.com>
2024-05-23 10:16:34 +0300

    Задание 3

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -5,20 +5,23 @@ public class Point {
     public double y;
     public double x1;
     public double y1;
- public Point(double x,double y,double x1,double y1){
-     this.x = x;
-     this.y =y;
-     this.x1 = x1;
-     this.y1 =y1;
- }
- public Point(double x, double y){
-     this.x= x;
-     this.y =y;
- }
- public double distance(){
-     double dx = this.x1 - this.x;
-     double dy = this.y1-this.y;
-     return Math.sqrt(dx*dx +dy*dy);
 
-}
+    public Point(double x, double y, double x1, double y1) {
+        this.x = x;
+        this.y = y;
+        this.x1 = x1;
+        this.y1 = y1;
+    }
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance() {
+        double dx = this.x1 - this.x;
+        double dy = this.y1 - this.y;
+        return Math.sqrt(dx * dx + dy * dy);
+
+    }
 }
